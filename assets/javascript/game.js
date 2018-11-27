7a0657d390ad3c45c018bebbce62912516cdddfd
Isabel <kuri.isabel@gmail.com>
2018-11-26 23:24:26 -0800

    updated file

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+<script src="javascript/assets/index.html"></script>
+
+var i = 0
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+
+letterdocument.getElementById("eachLetter").innerHTML = s[0].letters
+
+function printRandomLetter() {
+    console.log(Math.floor(Math.random());
+}
+
+function makeRandomLetter() {
+    return Math.floor(Math.random());
+}
+
+printRandomLetter();
+
+var randy = makeRandomLetter();
+
+document.onkeyup = function (event) {
+    // ...and use it again later on
+    randy++;
+
+    console.log(randy);
+
+letterdocument.getElementById("eachLetter").innerHTML = s[i].letters
+};
+
